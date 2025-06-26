3f68fee62abf09a8d275461943b041b514b091fb
jonasmelonas <128137429+jonasmelonas@users.noreply.github.com>
2025-06-26 13:54:26 +0900

    put, post og delete implementert

--- src/main/java/com/example/demo/PersonController.java
+++ src/main/java/com/example/demo/PersonController.java
@@ -2,14 +2,19 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collections;
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("/persons")
@@ -26,17 +31,70 @@ public class PersonController {
     }
 
     @GetMapping("/email/{email}")
-    public ResponseEntity<?> getByEmail(@PathVariable String email) {
+    public Person getByEmail(@PathVariable String email) {
         Person person = service.getByEmail(email);
         if (person == null) {
-            return ResponseEntity.ok(Collections.emptyMap());
-        } else {
-            return ResponseEntity.ok(person);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Person not found");
         }
+        return person;
     }
 
     @GetMapping("/search")
     public List<Person> getByNameSearch(@RequestParam String name) {
         return service.getByNameSearch(name);
+    }
+
+    @PostMapping("")
+    public Person createPerson(@RequestBody Person person) {
+
+        // gjør enkle valideringer av dataen
+        if (person.getFirstName() == null || person.getFirstName().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "First name is required");
+        }
+        if (person.getLastName() == null || person.getLastName().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Last name is required");
+        }
+        if (person.getEmail() == null || person.getEmail().isBlank() || !person.getEmail().contains("@")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A correct email is required");
+        }
+        if (person.getSalary() == null || person.getSalary().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Salary is required");
+        }
+
+        service.createPerson(person);
+        return person;
+    }
+
+    @PutMapping("/email/{email}")
+    public Person updatePerson(@PathVariable String email, @RequestBody Person person) {
+
+        // gjør enkle valideringer av dataen
+        if (person.getFirstName() == null || person.getFirstName().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "First name is required");
+        }
+        if (person.getLastName() == null || person.getLastName().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Last name is required");
+        }
+        if (person.getEmail() == null || person.getEmail().isBlank() || !person.getEmail().contains("@")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A correct email is required");
+        }
+        if (person.getSalary() == null || person.getSalary().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Salary is required");
+        }
+        
+        Person updatedPerson = service.updatePerson(email, person);
+        if(updatedPerson == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Person not found");
+        }
+        return updatedPerson;
+    }
+
+    @DeleteMapping("/email/{email}")
+    public Person deletePerson(@PathVariable String email) {
+        Person person = service.deletePerson(email);
+        if(person == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Person not found");
+        }
+        return person;
     }
 }
--- src/main/java/com/example/demo/PersonService.java
+++ src/main/java/com/example/demo/PersonService.java
@@ -15,7 +15,7 @@ import jakarta.annotation.PostConstruct;
 public class PersonService {
     private List<Person> persons = new ArrayList<>();
     private Map<String, Person> personsMap = new HashMap<>(); // for instant lookup på eposter som key
-    
+
     @PostConstruct
     public void loadDataFromFile() throws Exception {
         try (BufferedReader br = new BufferedReader(new FileReader("export.csv"))) {
@@ -23,7 +23,6 @@ public class PersonService {
             boolean header = true;
 
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
                 // skipper headeren
                 if (header) {
                     header = false;
@@ -38,19 +37,16 @@ public class PersonService {
                 p.setSalary(parts[3]);
                 p.setAge(Integer.parseInt(parts[4]));
 
-                System.out.println(p.toString());
-
                 // lagrer den nye personen i liste
                 persons.add(p);
 
-                // lagerer den nye personen også i map'et med epost som key
+                // lagrer den nye personen også i map'et med epost som key
                 personsMap.put(p.getEmail(), p);
             }
         }
     }
 
     public List<Person> getPersons() {
-        System.out.println(this.persons);
         return this.persons;
     }
 
@@ -66,7 +62,8 @@ public class PersonService {
         List<Person> results = new ArrayList<>();
 
         for (Person p : this.persons) {
-            // setter sammen fornavn og etternavn til en samlet streng som søket skal gjøres på
+            /* setter sammen fornavn og etternavn til en samlet streng som søket skal gjøres
+            på */
             StringBuilder fullName = new StringBuilder();
             fullName.append(p.getFirstName()).append(" ").append(p.getLastName());
 
@@ -75,5 +72,46 @@ public class PersonService {
             }
         }
         return results;
+    }
+
+    public void createPerson(Person person) {
+        this.persons.add(person);
+        this.personsMap.put(person.getEmail(), person);
+        // eventuelt oppdatere csv-fil
+    }
+
+    public Person updatePerson(String email, Person person) {
+        if(this.personsMap.containsKey(email)) {
+
+            //må finne person i arraylisten og erstatte den
+            for(Person p : persons) {
+                if(p.getEmail().equals(email)) {
+                    persons.remove(personsMap.get(email));
+                    persons.add(person);
+                    break;
+                }
+            }
+            //oppdaterer map'et og returnerer den nye versjonen
+            this.personsMap.remove(email);
+            this.personsMap.put(person.getEmail(), person);
+            return person;
+        }
+        return null;
+    }
+
+    public Person deletePerson(String email) {
+        if(this.personsMap.containsKey(email)) {
+
+            //må finne person i arraylisten og slette den
+            for(Person p : persons) {
+                if(p.getEmail().equals(email)) {
+                    persons.remove(personsMap.get(email));
+                    break;
+                }
+            }
+            //sletter fra map'et og returnerer
+            return this.personsMap.remove(email);
+        }
+        return null;
     }
 }
